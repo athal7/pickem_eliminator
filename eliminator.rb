a4a7ac1708f650a8f0d6b6bb0d14e98ed260b401
Andrew Thal <athal7@me.com>
2017-10-24 21:08:47 -0400

    Performance improvements and better pick recommendations

--- eliminator.rb
+++ eliminator.rb
@@ -1,9 +1,15 @@
 require 'csv'
+require 'thread'
 
 DATA = CSV.open("win_probabilities.csv", headers: true, header_converters: :symbol).read
 CURRENT_WEEK = ENV.fetch("CURRENT_WEEK", 1).to_i
-ITERATIONS = ENV.fetch("ITERATIONS", 100000).to_i
+ITERATIONS = ENV.fetch("ITERATIONS", 10000).to_i
 ALREADY_PICKED = ENV.fetch("ALREADY_PICKED", "").split(",")
+PICK_THRESHOLD = ENV.fetch("PICK_THRESHOLD", 50).to_i
+
+SEMAPHORE = Mutex.new
+WORK_QUEUE = Queue.new
+WORKERS = ENV.fetch("WORKERS", 4).to_i
 
 class Eliminator
   def initialize
@@ -16,15 +22,29 @@ class Eliminator
     best_set = {}
     best_probability = 0
 
-    ITERATIONS.times do |i|
-      pick_set = generate_pick_set
-      probability = probability(pick_set)
-      if probability > best_probability
-        best_set = pick_set
-        best_probability = probability
+    ITERATIONS.times { |i| WORK_QUEUE.push i }
+    workers = (0...WORKERS).map do
+      Thread.new do
+        begin
+          while WORK_QUEUE.pop(true)
+            pick_set = generate_pick_set
+            probability = probability(pick_set)
+            if probability > best_probability
+              SEMAPHORE.synchronize do
+                if probability > best_probability
+                  best_set = pick_set
+                  best_probability = probability
+                end
+              end
+            end
+          end
+        rescue ThreadError
+        end
       end
     end
+    workers.map(&:join)
 
+    puts "Score: #{best_probability}"
     (CURRENT_WEEK..17).each do |week|
       puts "Week #{week} pick: #{best_set[week][:team]}, pct: #{best_set[week][:probability]}"
     end
@@ -35,6 +55,7 @@ class Eliminator
     @chunked_data.each do |week, week_picks|
       while true do
         pick = week_picks.sample
+        next if pick[:probability].to_i < PICK_THRESHOLD
         existing_picks = picks.map { |week, existing_pick| existing_pick[:team] }
         unless existing_picks.include?(pick[:team]) || ALREADY_PICKED.include?(pick[:team])
           picks[week.to_i] = pick
